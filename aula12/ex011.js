7f194d7b4c505ba1d320db691239029a65af8c8e
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-02-27 13:04:15 -0300

    ex011, ex012 e ex013

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 22
+
+console.log(`Sua idade é ${idade} anos`)
+
+if (idade < 16){
+    console.log('Não pode votar.')
+} else if (idade < 18 || idade > 65){
+    console.log('Voto opcional')
+} else{
+    console.log('Voto obrigatório')
+}
