1d122feb5d534b3bb7b5a1a149e12bcfd9357ad8
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-03-01 17:36:54 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -3,13 +3,20 @@ num.push(45)
 num.sort(sortNumbers)
 
 /* Formas de usar estruturas de repetição para percorrer arrays */
-for(let pos = 0; pos < num.length; pos++){
+/* for(let pos = 0; pos < num.length; pos++){
     console.log(`Este elemento tem índice ${pos} e valor ${num[pos]}`)
-} 
+}  */
 
 /* for(let i in num){
     console.log(`Este elemento tem índice ${i} e valor ${num[i]}`)
 } */
+
+let index = num.indexOf(45)
+if(index == -1){
+    console.log(`Este número não está prsente no Array`)
+} else {
+    console.log(`O número escolhido está na posição ${index} do Array`)
+}
 
 function sortNumbers(a,b){
     if(a > b){
