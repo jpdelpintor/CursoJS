ae8155dc0801a08c9dccfb3190feaf9d31a6f088
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-03-22 11:08:26 -0300

    ex017

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,25 @@
+function calcular(){
+    let numeroForm = window.document.getElementById('txtnumber')
+    let res = window.document.getElementById('selectbox')
+    numero = Number(numeroForm.value)
+    let i = 1
+    let resp
+    
+
+    if(numeroForm.value.length == 0){
+        window.alert('Preencher o campo com um número')
+    } else{
+        res.innerHTML = ''
+        do {
+            let item = document.createElement('option')
+            item.text = `${numero} x ${i} = ${numero*i}`
+            item.value = `tab${i}`
+            res.appendChild(item)
+            i++
+        } while (i <= 10);
+    }
+
+    
+
+}
+
