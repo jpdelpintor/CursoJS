762859ff7658681332037640fae7e219c380734e
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-03-23 17:02:13 -0300

    ex018

--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -2,6 +2,7 @@ function adicionar(){
     let formNumero = window.document.getElementById('txtn')
     let res = window.document.getElementById('selectbox')
     let texto = res.options
+    let alarmeNumerorep = false
 
     
 
@@ -10,39 +11,47 @@ function adicionar(){
     } else if (res.length != 0){
 
         for (let i = 1; i <= res.length; i++){
-            window.alert(`O número na lista é ${texto.item(0).text}`)
-            /* if(Number(res.value[i-1].replace(/\D/g, '')) == Number(formNumero.value)){
-                window.alert(`Numeros iguais`)
-            }  */
+            
+            if(Number(texto.item(i-1).text.replace(/\D/g,'')) == Number(formNumero.value)){
+                alarmeNumerorep = true
+            } 
             
         }
-    }
-   
-    
-
-    //Verificando se o valor já está presente na SelectBox
-    /* if(res.length != 0){
-        for(let i = 1; i <= res.length; i++){
-            if(Number(res.value[i-1].replace(/\D/g, '')) == Number(formNumero.value)){
+        switch (alarmeNumerorep) {
+            case true:
                 window.alert('Valor inadequado ou repetido!')
-            }
-            
+                
+                break;
+        
+            default:
+                let item = window.document.createElement('option')
+                item.text = `valor ${Number(formNumero.value)} adicionado`
+                item.value = `item${res.length + 1}`
+                res.appendChild(item)
+                break;
         }
-        let item = window.document.createElement('option')
-        item.text = `valor ${Number(formNumero.value)} adicionado`
-        item.value = `item${res.length + 1}`
-        res.appendChild(item)
+        
+
     } else{
         let item = window.document.createElement('option')
         item.text = `valor ${Number(formNumero.value)} adicionado`
         item.value = `item${res.length + 1}`
         res.appendChild(item)
-
-    } */
-
-
+    }
+   
     
-    
+}
+
+function finalizar(){
+    let caixaSelecao = window.document.getElementById('selectbox')
+    let texto = caixaSelecao.options
+
+    if (texto.length == 0){
+        window.alert('Não tem valor na caixa de seleção')
+    } else {
+        
+
+    }
 }
 
 
