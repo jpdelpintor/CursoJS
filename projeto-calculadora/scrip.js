c9151077ca29f89fbf1d971a5583d553070a3a28
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-04-05 18:01:27 -0300

    script.js update

---
+++ projeto-calculadora/scrip.js
@@ -0,0 +1,5 @@
+//Selecionando elementos do DOM
+const selecaoAtual = window.document.querySelector('div#selecao-atual')
+const selecaoAnterior = window.document.querySelector('div#selecao-anterior')
+const button = window.document.querySelectorAll('div#calculadora button')
+
