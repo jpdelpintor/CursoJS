20b1d36166e4888476f207b2c9f4b8055b01e307
João Pedro Ferreira Del Pintor <76700229+jpdelpintor@users.noreply.github.com>
2023-04-18 15:03:19 -0300

    update projeto-calculadora

--- projeto-calculadora/script.js
+++ projeto-calculadora/script.js
@@ -1,7 +1,57 @@
 //Selecionando elementos do DOM
-const selecaoAtual = window.document.querySelector("div#selecao-atual")
-const selecaoAnterior = window.document.querySelector("div#selecao-anterior")
+const previousOperationText = document.querySelector('div#operacao-anterior')
+const currentOperationText = document.querySelector('div#operacao-atual')
 const buttons = window.document.querySelectorAll("#calculadora button")
+
+class Calculator{
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+    }
+
+    addDigit(digit){
+        //Evitando que o usuário digite mais de um ponto
+        if(digit === ',' && this.currentOperationText.innerText.includes(',')){
+            return
+        }
+
+        this.currentOperation = digit
+        this.updateScreen()
+
+    }
+
+    
+    //Processando as operações
+    processOperation(operation){
+        let operationValue
+        const previous = +this.previousOperationText.innerText
+        const current = +this.currentOperationText.innerText
+
+        switch(operation){
+            case '+':
+                operationValue = previous + current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            default:
+                break
+
+        }
+
+    }
+
+    //Adicionar os valores na tela da calculadora
+    updateScreen(
+        operationValue = null, 
+        operation=null, 
+        current=null, 
+        previous = null){
+        this.currentOperationText.innerText += this.currentOperation
+    }
+
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText)
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -9,9 +59,9 @@ buttons.forEach((btn) => {
         
         //Separando número e ponto de operação
         if (+value >= 0 || value === ','){
-            console.log(`Valor numério: ${value}`)
+            calc.addDigit(value)
         } else{
-            console.log(`Operação: ${value}`)
+            calc.processOperation(value)
         }
     })
 })
